8cad887904d0ecbd548c51bcf26d506a587e6eeb
Bernadette Angelice Novena Octaviani <belajarcodingangelicenovenaoct@gmail.com>
2025-12-25 10:42:07 +0700

    Create BigData.java

---
+++ BigData.java
@@ -0,0 +1,9 @@
+public class BigData {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Line ke-" + i + ": Java sedang berusaha muncul di grafik!");
+            System.out.println("Menambah baris kode supaya persentase naik.");
+            System.out.println("Java is powerful and red!");
+        }
+    }
+}
