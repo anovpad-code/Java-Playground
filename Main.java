3307e834e3fb30f2b91f49e2524b50223433e546
Bernadette Angelice Novena Octaviani <belajarcodingangelicenovenaoct@gmail.com>
2025-12-25 08:48:43 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Java! Just playing around here.");
+        int a = 10;
+        int b = 20;
+        System.out.println("Result: " + (a + b));
+    }
+}
